2e126843ca2c0119c69d8fb3c1d7d9027c601a32
matrixcodebreaker <64915527+matrixcodebreaker@users.noreply.github.com>
2020-09-17 09:45:50 -0400

    Update multiplyArrElements.js

--- multiplyArrElements.js
+++ multiplyArrElements.js
@@ -1,62 +1,6 @@
-// console.log(Math.floor(1.5))
-// console.log(Math.floor(2.3))
-// // console.log(Math.ciel(1.5))
-// // console.log(Math.ciel(2.3))
-// console.log(Math.random())
-// console.log(Math.random())
-
-// console.log(Math.sign(3))
-// console.log(Math.sign(-3))
-
-// console.log(Math.max(1,2,3,66))
-// console.log(Math.min(1, 4, -3))
-// console.log(Math.pow(4, 2))
-
-// let array = [1,2,3]
-// for (var value of array) {     
-//   // console.log(value)
-// }
-
-// console.log(array.shift())
-// // console.log(array.shift())
-// console.log(array)
-
-//Challenge 11
-// var a = [1,2,3];
-// var b = a;
-// console.log("array a is equal to array b?", a == b) // what will this return?
-// var c = [1,2,3]
-// console.log("array a is equal to array c?", a == c) // what will this return?
-// //does this return false because it is being saved to a different place in memory?
-
-// var d = a.slice();
-// console.log("array a is equal to array d?", a == d) // what will this return?
-
-// var sliceMe = ["first","half", "second", "half"];
-// var firstHalf =  sliceMe.slice(0, 2)
-// var secondHalf = sliceMe.slice(2,4)
-// console.log(firstHalf)
-// console.log(secondHalf)
-// console.log(sliceMe)
-// //splice DOES mutate the array unlike slice.
-// splice: (start after breakeup, remove absolutely all memory of their ex, input love , input performance, input consciousness)
-// //splice returns an array OF THE REMOVED ELEMENTS 
-// //12
-
-// var arrA = [0,1,"remove me!", 2, 3]
-// arrA.splice(2,1); 
-// console.log(arrA); //0,1,2,3
-// arrA.splice(1,1,"one"); //0, "one", "2, 3"
-// console.log(arrA);
-
-// var numCount = [0, 1, "two", "three", 4];
-// var wordCount = ["zero", "one", 2, 3, "four"];
-
-// numCount.splice(2, 2, 2, 3)
-// // console.log(numCount)
-// wordCount.splice(2, 2)
-// // wordCount.splice(4, 1, 4)
-// console.log(wordCount)
+Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it.
+For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24. For the test cases,
+the range will be between 1 and 18 and the input will always be an integer.
 
 function FirstFactorial(num) {
 
@@ -77,9 +21,4 @@ function FirstFactorial(num) {
   }
   console.log(FirstFactorial(8));
   
- 
- 
- 
- 
-    
- // keep this function call here 
+
